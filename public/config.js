fa70436bd67165bf5b8dc08b14115fc06280be70
HGD7764 <65435258+HGD7764@users.noreply.github.com>
2023-02-08 17:30:36 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,36 +1,27 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'GStarSky状态监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
-  ],
+  ApiKeys: [ 'u2010555-1e760472db67f99c6c77685c', ]
 
   // 日志天数
   CountDays: 90,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '主站',
+      url: 'https://www.bestglss.fun/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+      text: '云盘',
+      url: 'https://cloud.bestglss.fun/'
+    }，
   ],
 };
