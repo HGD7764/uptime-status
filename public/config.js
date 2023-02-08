f50303426168f6e51d823c765fd1283c1d265412
HGD7764 <65435258+HGD7764@users.noreply.github.com>
2023-02-08 17:48:39 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,24 +1,24 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'GStarSky状态监测',
+  SiteName: 'GStarSky服务器状态监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [ 
-    'm793670465-6f120324f1fd6b8330201ab5', 
-  ]
+  ApiKeys: [
+    'm793670465-6f120324f1fd6b8330201ab5',
+  ],
 
   // 日志天数
   CountDays: 90,
 
   // 是否显示检测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 导航栏菜单
   Navi: [
     {
-      text: '主站',
+      text: '主页',
       url: 'https://www.bestglss.fun/'
     },
   ],
